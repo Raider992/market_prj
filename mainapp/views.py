5eddac58ddbdb6a48769f650a74c56868f3b853c
Raider992 <19stargazer92@gmail.com>
2025-06-01 20:24:26 +0300

    End of Lesson 3, everything works so far. Moving to creation of the branch for the next lesson.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Accommodation
 
-# Create your views here.
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+def accommodations(request):
+    title = 'отели'
+
+    list_of_accommodations = Accommodation.objects.filter(is_active=True)
+
+    context = {
+        'title':title,
+        'list_of_accommodations':list_of_accommodations
+    }
+
+    return render(request, 'mainapp/accommodations.html', context)
+
+
+def accommodation(request, pk):
+    title = 'отель'
+
+    context = {
+        'title':title,
+        'accommodation': get_object_or_404(Accommodation, pk=pk),
+    }
+
+    return render(request, 'mainapp/accomodation_details.html', context)
